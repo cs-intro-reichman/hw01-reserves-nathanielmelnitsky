8d13aafff11e7a34fd513a20e54c043af83d0025
nathanielmelnitsky <nmelnitsky@gmail.com>
2024-02-13 14:03:57 +0200

    first attempt

--- Coins.java
+++ Coins.java
@@ -1,9 +1,10 @@
-/*
- * Write a program that gets a quantity of cents as a command-line argument.
- * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
- */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+
+		int given = Integer.parseInt(args[0]);
+		int remain = given % 25;
+		int quarters = given / 25;
+		System.out.println("Use " + quarters + " quarters " + remain + " cents");
+
 	}
 }
--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,40 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		//recieves range values
+		int min = Integer.parseInt(args[0]);
+		int max = Integer.parseInt(args[1]);
+
+		// generates random doubles for each generated number
+		double multVal1 = Math.random();
+		double multVal2 = Math.random();
+		double multVal3 = Math.random();
+
+		//gen 3 random numbers
+		int num1 = (int)(min + (multVal1*(max - min)));
+		System.out.println(num1);
+
+		int num2 = (int)(min + (multVal2*(max - min)));
+		System.out.println(num2);
+
+		int num3 = (int)(min + (multVal3*(max - min)));
+		System.out.println(num3);
+
+		//finds lowest val and prints
+		int tester = num1;
+
+		if(num2 < tester){
+			tester = num2;
+		}
+		if(num3 < tester){
+			tester = num3;
+		}
+		System.out.println("The minimal generated number was " + tester);
+
+
+
+
+
+
 	}
 }
